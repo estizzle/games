4c4efa93734332218a2b0f8d1c673da2c1eb072b
Eston <estoncjunior@gmail.com>
2023-10-30 16:52:02 -0400

    basic set up files - package, webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['./client/index.js'],
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js',
+    },
+    context: __dirname,
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      ],
+    },
+  };
+  
